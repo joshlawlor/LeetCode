13de6a1b452b1afd603a0dcb314e57f10c60eb57
Josh Lawlor <joshlawlor1@gmail.com>
2023-02-15 12:45:36 -0500

    Initial Twosum problem setup

---
+++ TwoSum/twosum.js
@@ -0,0 +1,17 @@
+
+//EXAMPLE 1
+const nums = [2,7,11,15]
+const target = 9
+
+//EXAMPLE 2
+// const nums = [3,2,4]
+// const target = 6
+
+//EXAMPLE 3
+// const nums = [3,3]
+// const target = 6
+
+
+var twoSum = function(nums, target) {
+
+}
