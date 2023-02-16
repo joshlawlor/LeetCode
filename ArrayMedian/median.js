410e1913836362edd92b1c1d9ee8bc82fafe60bb
Josh Lawlor <joshlawlor1@gmail.com>
2023-02-16 18:50:00 -0500

    Correct median displayed for all examples

--- ArrayMedian/median.js
+++ ArrayMedian/median.js
@@ -1,12 +1,12 @@
 
-var findMedian = function(nums1=[1,3],nums2=[2]){
+var findMedian = function(nums1=[1,2],nums2=[3,4]){
 const merged = [...nums1, ...nums2]
 console.log(merged)
 
 const median = arr => {
     const mid = Math.floor(arr.length / 2),
     nums = [...arr].sort((a,b) => a-b);
-    return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid] / 2)
+    return arr.length % 2 !== 0 ? nums[mid] : (nums[mid - 1] + nums[mid]) / 2
 }
 
 console.log(median(merged))
