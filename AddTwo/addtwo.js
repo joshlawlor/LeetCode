e3d0c1040f5241c648f9e25dbcaac6df7d3fb30b
Josh Lawlor <joshlawlor1@gmail.com>
2023-02-16 14:12:42 -0500

    Fooling around with typescript compiler

--- AddTwo/addtwo.js
+++ AddTwo/addtwo.js
@@ -1,26 +1,30 @@
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
-/**
- * 
-
-Example 1:
-Input: l1 = [2,4,3], l2 = [5,6,4]
-Output: [7,0,8]
-Explanation: 342 + 465 = 807.
-
-Example 2:
-Input: l1 = [0], l2 = [0]
-Output: [0]
-
-Example 3: 
-Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
-Output: [8,9,9,9,0,0,0,1]
- */
-var addTwoNumbers = function(l1=[2,4,3], l2=[5,6,4]) {
-    
-};
+var ListNode = /** @class */ (function () {
+    function ListNode(val, next) {
+        this.val = (val === undefined ? 0 : val);
+        this.next = (next === undefined ? null : next);
+    }
+    return ListNode;
+}());
+function addTwoNumbers(l1, l2) {
+    var remainder = 0;
+    var current = new ListNode();
+    var result = current;
+    while (l1 !== null || l2 !== null || remainder > 0) {
+        var sum = remainder;
+        if (l1) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+        if (l2) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+        remainder = Math.floor(sum / 10);
+        current.next = new ListNode(Math.floor(sum % 10));
+        current = current.next;
+    }
+    return result.next;
+}
+;
+addTwoNumbers();
+console.log('Hello');
---
+++ AddTwo/addtwo.ts
@@ -0,0 +1,35 @@
+class ListNode {
+         val: number 
+         next: ListNode | null
+         constructor(val?: number, next?: ListNode | null) {
+             this.val = (val===undefined ? 0 : val)
+             this.next = (next===undefined ? null : next)
+         }
+    }
+
+
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+    let remainder = 0;
+    let current = new ListNode();
+    let result = current;
+    
+    while (l1 !== null || l2 !== null || remainder > 0) {
+        let sum = remainder;
+        if (l1) {
+            sum += l1.val;
+            l1 = l1.next;
+        }
+        if (l2) {
+            sum += l2.val;
+            l2 = l2.next;
+        }
+        
+        remainder = Math.floor(sum / 10);
+        current.next = new ListNode(Math.floor(sum % 10));
+        current = current.next;
+    }
+    return result.next;
+};
+addTwoNumbers()
+console.log('Hello')
