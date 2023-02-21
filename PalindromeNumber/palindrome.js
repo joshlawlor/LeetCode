626a06135cc7a8ec82c10f4ab60998414e086d61
Josh Lawlor <joshlawlor1@gmail.com>
2023-02-20 21:13:15 -0500

    Pseudocode done. Have my two arrays, NaN should be sufficient for current testcase. Writing validate function next

--- PalindromeNumber/palindrome.js
+++ PalindromeNumber/palindrome.js
@@ -1,7 +1,19 @@
 
-const num = 121
+const x = 121
 
 
-var isPalindrom = function(nums){
+var isPalindrome = function(x){
+
+    const leftRight = (""+x).split("").map(Number).reverse();
+    const rightLeft = leftRight.reverse()
+    console.log(leftRight, rightLeft)
+    
     
 }
+
+isPalindrome(-121)
+
+//First, need to map over the array, and push each letter into two seperate arrays
+// The first array in regular order, the second array reversed order
+// Then compare those two arrays, if they match it is palindrome
+ 
