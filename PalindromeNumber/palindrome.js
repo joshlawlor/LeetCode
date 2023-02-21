5ffe13e911b37698edb4e9d4867c212d6be3df74
Josh Lawlor <joshlawlor1@gmail.com>
2023-02-20 21:28:56 -0500

    For loop solution works for simple test case. But not more complex. Still refactoring

--- PalindromeNumber/palindrome.js
+++ PalindromeNumber/palindrome.js
@@ -2,16 +2,38 @@
 const x = 121
 
 
-var isPalindrome = function(x){
+var isPalindrome = function(){
 
     const leftRight = (""+x).split("").map(Number).reverse();
-    const rightLeft = leftRight.reverse()
+    const rightLeft = (""+x).split("").map(Number);
+
+
+    const array = Array.from(x.toString()).map(Number).reverse()
+    const array2 = Array.from(x.toString()).map(Number)
+
+
+    console.log('Array'+array+array2)
+
     console.log(leftRight, rightLeft)
     
-    
+    // if(array != array2){
+    //     return console.log(false)
+    // }
+    // else{
+    //     return console.log(true)
+    // }
+
+
+    for (let i = 0; i < array.length; i++){
+        if (array[i] != array2[i]){
+            return console.log(false)
+        }else {
+            return console.log(true)
+        }
+    }
 }
 
-isPalindrome(-121)
+isPalindrome(x)
 
 //First, need to map over the array, and push each letter into two seperate arrays
 // The first array in regular order, the second array reversed order
