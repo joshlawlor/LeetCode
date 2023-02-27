c0d5501b02d0ade0a7625863da912ac67a20d762
Josh Lawlor <joshlawlor1@gmail.com>
2023-02-27 17:57:18 -0500

    Solution for first three testcase complete

--- RomanToInteger/roman.js
+++ RomanToInteger/roman.js
@@ -1,8 +1,26 @@
-const s = III
+const s =["I", "I", "I"]
+
+const romanValues =
+{
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+}
 
 
 
 var romanToInt = function(s) {
+
+    answer = 0;
+
+    for(let i = 0; i < s.length; i+=1){
+        romanValues[s[i]] < romanValues[s[i+1]] ? answer -= romanValues[s[i]]: answer += romanValues[s[i]]
+    }
+    return console.log(answer)
 
     //First I am going to define a set of values that align with the correct roman integer
     // Then inside my function, I will go through the entire test case array with a for loop
@@ -10,3 +28,5 @@ var romanToInt = function(s) {
     // my running answer 
     
 };
+
+romanToInt(s)
