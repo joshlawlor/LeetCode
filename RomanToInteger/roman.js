fb5cdaa39bb990cdca2fdb68a1db1cfb10f23601
Josh Lawlor <joshlawlor1@gmail.com>
2023-02-27 17:48:21 -0500

    Added pseudocode for solution

--- RomanToInteger/roman.js
+++ RomanToInteger/roman.js
@@ -3,5 +3,10 @@ const s = III
 
 
 var romanToInt = function(s) {
+
+    //First I am going to define a set of values that align with the correct roman integer
+    // Then inside my function, I will go through the entire test case array with a for loop
+    // I will compare the s[i] with my roman values, if there is a match it will add that value to
+    // my running answer 
     
 };
