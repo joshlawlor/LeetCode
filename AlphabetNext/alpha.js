1a367e62f7fd3ab4a959f36d02a05f21b51f0f40
Josh Lawlor <joshlawlor1@gmail.com>
2023-03-02 19:46:56 -0500

    Solution added

--- AlphabetNext/alpha.js
+++ AlphabetNext/alpha.js
@@ -1,6 +1,21 @@
 
 const alphabet = function(x){
     console.log(x)
+    let index = 0
+    let answer = ""
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    for(let i = 0; i < x.length; i++){
+        
+        if(x == "z"){
+            answer += "a"
+        }else{
+            index = alphabet.indexOf(alphabet[i])+1
+            answer += alphabet[index]
+        }
+
+    }
+    console.log(answer)
 }
 
 
