aa61f7988ebbc439a448f74f56da7c9e1e43439c
Josh Lawlor <joshlawlor1@gmail.com>
2023-11-03 21:08:45 -0700

    Python Array Remove Duplicate problem setup

--- LongestPalindrome/longest.js
+++ LongestPalindrome/longest.js
@@ -1,4 +1,16 @@
+
+
+
 var longestPalindrome = function(s) {
 
-    
+    for (let i=0; i< s.length; i++){
+        console.log(s[i])
+    }   
+
+
+    console.log(s)
+
 }
+
+
+longestPalindrome("babad")
