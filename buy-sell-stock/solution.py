8c9ba7ae46ef3fd9b477d138c714e86ba9e5062d
Josh Lawlor <joshlawlor1@gmail.com>
2023-11-07 21:53:05 -0800

    Buy and Sell stock solution

--- buy-sell-stock/solution.py
+++ buy-sell-stock/solution.py
@@ -4,4 +4,10 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
+        max_profit = 0
         
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                max_profit += prices[i] - prices[i - 1]
+        
+        return max_profit
