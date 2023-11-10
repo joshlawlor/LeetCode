ee567d69a4bcf62c9cf8188e0a78af6d21e36c57
Josh Lawlor <joshlawlor1@gmail.com>
2023-11-09 19:49:41 -0800

    Intersection of Two Arrays solution

--- intersection-array/solution.py
+++ intersection-array/solution.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 class Solution(object):
     def intersect(self, nums1, nums2):
         """
@@ -5,4 +7,16 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        
+        # Count occurrences of each element in nums1 and nums2
+        count1 = Counter(nums1)
+        count2 = Counter(nums2)
+
+        # Find the common elements and their counts
+        common_elements = count1 & count2
+
+        # Build the result list
+        result = []
+        for num, count in common_elements.items():
+            result.extend([num] * count)
+
+        return result
