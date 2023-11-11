ea7234db85cb5bd3f41e1cb29dc76f1d137800d1
Josh Lawlor <joshlawlor1@gmail.com>
2023-11-10 23:28:14 -0800

    Move zeros solution

--- move-zeros/solution.cpp
+++ move-zeros/solution.cpp
@@ -1,6 +1,18 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        
+        int nonZeroIndex = 0;
+
+        // Move non-zero elements to the front
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] != 0) {
+                nums[nonZeroIndex++] = nums[i];
+            }
+        }
+
+        // Fill the remaining positions with zeros
+        for (int i = nonZeroIndex; i < nums.size(); ++i) {
+            nums[i] = 0;
+        }
     }
 };
