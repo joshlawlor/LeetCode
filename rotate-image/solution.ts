3bff8c27741c678dfecf6a00129cc7badeb3ee63
Josh Lawlor <joshlawlor1@gmail.com>
2023-11-15 17:05:02 -0800

    Rotate Image solution

--- rotate-image/solution.ts
+++ rotate-image/solution.ts
@@ -1,6 +1,15 @@
-/**
- Do not return anything, modify matrix in-place instead.
- */
- function rotate(matrix: number[][]): void {
-    
- };
+function rotate(matrix: number[][]): void {
+    const n = matrix.length;
+
+    // Transpose the matrix
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j < n; j++) {
+            [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
+        }
+    }
+
+    // Reverse each row
+    for (let i = 0; i < n; i++) {
+        matrix[i].reverse();
+    }
+}
