10b3e661f6679b482d2958f8c4c0bd68268fa770
Josh Lawlor <joshlawlor1@gmail.com>
2023-11-16 19:04:18 -0800

    Delete Node solution

--- linked-list-delete-node/solution.py
+++ linked-list-delete-node/solution.py
@@ -10,4 +10,7 @@ class Solution(object):
         :type node: ListNode
         :rtype: void Do not return anything, modify node in-place instead.
         """
-        
+        # Copy the value of the next node to the current node
+        node.val = node.next.val
+        # Update the next pointer to skip the next node
+        node.next = node.next.next
