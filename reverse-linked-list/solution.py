e7d6aa34583be5bccc9b5d46ff88c16e697c48f6
Josh Lawlor <joshlawlor1@gmail.com>
2023-11-17 19:02:00 -0800

    Reverse Linked List solution

--- reverse-linked-list/solution.py
+++ reverse-linked-list/solution.py
@@ -3,10 +3,22 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution(object):
     def reverseList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
         """
-        
+        # Base case: empty list or last node reached
+        if head is None or head.next is None:
+            return head
+
+        # Reverse the rest of the list
+        reversed_list = self.reverseList(head.next)
+
+        # Adjust pointers for the current node
+        head.next.next = head
+        head.next = None
+
+        return reversed_list
