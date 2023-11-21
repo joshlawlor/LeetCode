d7ac0908c8ffda0c6bd6459dc0a1cbe633e356ca
Josh Lawlor <joshlawlor1@gmail.com>
2023-11-20 16:20:15 -0800

    Palindrome L-L solution

--- palindrome-linked-list/solution.py
+++ palindrome-linked-list/solution.py
@@ -1,12 +1,42 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
 class Solution(object):
     def isPalindrome(self, head):
         """
         :type head: ListNode
         :rtype: bool
         """
-        
+        # Helper function to reverse a linked list
+        def reverse_list(node):
+            prev = None
+            while node:
+                next_node = node.next
+                node.next = prev
+                prev = node
+                node = next_node
+            return prev
+
+        # Helper function to find the middle of the linked list
+        def find_middle(node):
+            slow = node
+            fast = node
+            while fast and fast.next:
+                slow = slow.next
+                fast = fast.next.next
+            return slow
+
+        if not head or not head.next:
+            return True  # Empty or single-node list is a palindrome
+
+        # Find the middle of the linked list
+        middle = find_middle(head)
+
+        # Reverse the second half of the linked list
+        reversed_second_half = reverse_list(middle)
+
+        # Compare the first half with the reversed second half
+        while reversed_second_half:
+            if head.val != reversed_second_half.val:
+                return False
+            head = head.next
+            reversed_second_half = reversed_second_half.next
+
+        return True
