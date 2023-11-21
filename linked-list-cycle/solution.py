7674a92c665a94a16e92fd50d1bf510a22e5f58f
Josh Laptop <joshlawlor1@gmail.com>
2023-11-20 17:11:55 -0800

    Linked-List Cycle solution

--- linked-list-cycle/solution.py
+++ linked-list-cycle/solution.py
@@ -10,4 +10,20 @@ class Solution(object):
         :type head: ListNode
         :rtype: bool
         """
-        
+        # Base case: empty list or a list with only one node
+        if not head or not head.next:
+            return False
+
+        # Initialize two pointers, slow and fast
+        slow = head
+        fast = head.next
+
+        # Move the pointers until they meet or fast reaches the end of the list
+        while fast and fast.next:
+            if slow == fast:
+                return True
+            slow = slow.next
+            fast = fast.next.next
+
+        # If fast reaches the end, there is no cycle
+        return False
